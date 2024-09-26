dea540e335b25fe2393f8d8c4c36d9372c68a74e
Artem <shayazdanov@mybook.ru>
2024-09-26 20:26:02 +0300

    тестовый push через vscode

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import {invertCase} from './invertCase.js';
 import fizzBuzz from './fizzBuzz.js';
 
-//invertCase('I loVsssse JS'); // меняем регистр у букв
+invertCase('I loVsssse JS'); // меняем регистр у букв
 
 fizzBuzz(11, 20); // выводит в терминал числа в диапазоне от begin до end (остальные условия смотрите в файле fizzBuzz.js)
